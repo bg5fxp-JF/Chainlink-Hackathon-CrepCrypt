c1c233e372fa3f74f22c54ea2a3b1266a86b06d5
bg5fxp-JF <jacobfecunda@outlook.com>
2023-11-26 15:21:04 +0000

    added metadata

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,12 +2,6 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 
-export const metadata = {
-	title: "CrepCrypt",
-	description:
-		"A decentralized marketplace that utilizes AI and cutting-edge blockchain technology to ensure secure ownership of listed shoes.",
-};
-
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,13 @@
-import Image from "next/image";
 import ShuffleHero from "./components/ShuffleHero";
 import DiscoverSection from "./components/DiscoverSection";
 import RevolutionarySection from "./components/RevolutionarySection";
 import FAQSection from "./components/FAQSection";
+
+export const metadata = {
+	title: "CrepCrypt",
+	description:
+		"A decentralized marketplace that utilizes AI and cutting-edge blockchain technology to ensure secure ownership of listed shoes.",
+};
 
 export default function Home() {
 	return (
