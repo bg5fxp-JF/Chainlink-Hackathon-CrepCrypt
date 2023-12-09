78e40c203d2d7649c3ac7a920bed982f4b1adbea
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-09 16:27:53 +0000

    slight style changes

--- frontend/src/app/approval/page.js
+++ frontend/src/app/approval/page.js
@@ -1,5 +1,11 @@
-import Approval from './components/Approval'
+import Approval from "./components/Approval";
+
+export const metadata = {
+	title: "CrepCrypt | Approval",
+	description:
+		"A decentralized marketplace that utilizes AI and cutting-edge blockchain technology to ensure secure ownership of listed shoes.",
+};
 
 export default function page() {
-    return <Approval />;
+	return <Approval />;
 }
